be36bd10616039aaa1d73e81ed72af53d8392bb1
shkwahid <148443623+shkwahid@users.noreply.github.com>
2023-10-22 12:25:02 +0530

    Add files via upload

---
+++ employee-manager/src/main/java/com/employee/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.employee.service;
+
+import com.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
+    Employee getEmployeeById(long id);
+    Employee updateEmployee(Employee employee, long id);
+    void deleteEmployee(long id);
+}
+
